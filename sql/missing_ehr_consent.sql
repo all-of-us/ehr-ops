b6da6527a26f2c4359da85176f2472eb188bd0ae
nripya <na2960@cumc.columbia.edu>
2021-10-27 17:21:20 -0400

    updated changes

--- sql/missing_ehr_consent.sql
+++ sql/missing_ehr_consent.sql
@@ -1,26 +1,35 @@
-SELECT
+
+SELECT 
 src_hpo_id,
 Count(distinct(person_id)) as Missing_EHR_Consent_Count,
-sum(withdrawn) as withdrawn_status,
-sum(no_value_consent) as wrong_consent
+sum(withdrawn_before_submission) as withdrawn_before_sub,
+sum(withdrawn_after_submission) as withdrawn_after_sub,
+sum(no_consent_but_submitted) as consent_no_but_submitted,
+sum(removed_consent_since) as consent_removed_since_last_submission
 FROM (
-    SELECT
+    SELECT 
     person_id,
     src_hpo_id,
     consent_module,
     consent_value,
     consent_module_authored,
     withdrawal_time,
-    CASE WHEN withdrawal_time IS NOT NULL THEN 1 ELSE 0
-        END AS withdrawn,
-    CASE WHEN withdrawal_time is NULL and consent_value != 'ConsentPermission_Yes' THEN 1 ELSE 0
-        END AS no_value_consent,
+    person_upload_time,
+    CASE WHEN withdrawal_time IS NOT NULL AND (DATE(withdrawal_time) < DATE(person_upload_time)) THEN 1 ELSE 0
+    END AS withdrawn_before_submission,
+    CASE WHEN withdrawal_time IS NOT NULL AND (DATE(withdrawal_time) > DATE(person_upload_time)) THEN 1 ELSE 0
+    END AS withdrawn_after_submission,
+    CASE WHEN withdrawal_time is NULL and consent_value != 'ConsentPermission_Yes' AND (DATE(consent_module_authored) < DATE(person_upload_time)) THEN 1 ELSE 0
+    END AS no_consent_but_submitted,
+    CASE WHEN withdrawal_time is NULL and consent_value != 'ConsentPermission_Yes' AND (DATE(consent_module_authored) > DATE(person_upload_time)) THEN 1 ELSE 0
+    END AS removed_consent_since,
     rank() over (partition by person_id order by consent_module_authored desc) d_order
-        FROM {{pdr_project}}.{{rdr_ops_dataset}}._mapping_person as ehr
-        INNER JOIN `{{pdr_project}}.{{rdr_ops_dataset}}.v_pdr_participant_all` pdr_all
+    FROM {{pdr_project}}.{{rdr_ops_dataset}}._mapping_person as ehr
+    INNER JOIN `{{pdr_project}}.{{rdr_ops_dataset}}.table_counts_with_upload_timestamp_for_hpo_sites` rsubmission
+        ON ehr.src_hpo_id = lower(rsubmission.hpo_id)
+    INNER JOIN `{{pdr_project}}.{{rdr_ops_dataset}}.v_pdr_participant_all` pdr_all
         ON ehr.person_id = pdr_all.participant_id
-        LEFT JOIN `{{pdr_project}}.{{rdr_ops_dataset}}.v_pdr_participant_consent` pdr_cons
-        ON person_id = pdr_cons.participant_id
-        WHERE consent_module ='EHRConsentPII')
-WHERE d_order = 1 and consent_value != 'ConsentPermission_Yes'
-GROUP BY 1
+    LEFT JOIN `{{pdr_project}}.{{rdr_ops_dataset}}.v_pdr_participant_consent` pdr_cons
+        ON person_id = pdr_cons.participant_id AND consent_module = 'EHRConsentPII')
+WHERE d_order = 1 and (consent_value != 'ConsentPermission_Yes' or withdrawal_time is not null) 
+Group by 1
