efbdc4c4140d319a2c98eb54c59234b9ad6f497c
zoeyjiang <xj2193@cumc.columbia.edu>
2022-08-19 13:16:19 -0400

    Update physical measurement query with `

--- dashboard_metrics/EHR Ops General Data Quality Dashboard/sql/ehr_metrics/general_metrics/physical_meas.sql
+++ dashboard_metrics/EHR Ops General Data Quality Dashboard/sql/ehr_metrics/general_metrics/physical_meas.sql
@@ -3,56 +3,56 @@
 with
 person as (
 select distinct mp.src_hpo_id, mp.person_id
-from (select DISTINCT *  from {{curation_project}}.{{ehr_ops_dataset}}._mapping_person) mp
-join {{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_person p using(person_id)
+from (select DISTINCT *  from `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person`) mp
+join `{{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_person` p using(person_id)
 ),
 visit as (
 select distinct mv.src_hpo_id, v.person_id
-from {{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_visit_occurrence v
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_visit_occurrence mv using(visit_occurrence_id)
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_person p on p.person_id = v.person_id and p.src_hpo_id = mv.src_hpo_id
+from `{{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_visit_occurrence` v
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_visit_occurrence` mv using(visit_occurrence_id)
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person` p on p.person_id = v.person_id and p.src_hpo_id = mv.src_hpo_id
 ),
 site as (
 select distinct
 mp.person_id,
 mp.src_hpo_id
-from {{curation_project}}.{{ehr_ops_dataset}}._mapping_person mp
+from `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person` mp
 ),
 measurement as (
 select distinct mp.src_hpo_id, m.person_id
-from {{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement m
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement mp using(measurement_id)
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_person p on p.person_id = m.person_id and p.src_hpo_id = mp.src_hpo_id
+from `{{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement` m
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement` mp using(measurement_id)
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person` p on p.person_id = m.person_id and p.src_hpo_id = mp.src_hpo_id
 ),
 height as (
 select distinct mp.src_hpo_id, m.person_id
-from {{curation_project}}.{{ehr_ops_dataset}}._mapping_person mp
-join {{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement m on mp.person_id = m.person_id
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
+from `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person` mp
+join `{{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement` m on mp.person_id = m.person_id
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement` mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
 where m.measurement_concept_id in (3036277,1029031,3019171,3014149,3023540,37020651,3035463,
 3015514,3036798,3013842,3008989,1003304,37020737,1003232,1003850,4030731)
 ),
 weight as (
 select distinct mp.src_hpo_id, m.person_id
-from {{curation_project}}.{{ehr_ops_dataset}}._mapping_person mp
-join {{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement m on mp.person_id = m.person_id
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
+from `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person` mp
+join `{{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement` m on mp.person_id = m.person_id
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement` mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
 where m.measurement_concept_id in (3013853,3015644,3026600,3011043,3023166,40759214,40759213,3019336,46234683,40760186,40771968,3010147,21492642,
 1004141,1003261,43533987,40761330,1004122,3010220,3011054,40771967,3013131,3022281,3009617,3005422,3013762,3027492,3021838,3028543,45876172,
 1029318,45876173,3025315,45876171,3042378,3010914,3013747,3026659)
 ),
 bmi as (
 select distinct mp.src_hpo_id, m.person_id
-from {{curation_project}}.{{ehr_ops_dataset}}._mapping_person mp
-join {{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement m on mp.person_id = m.person_id
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
+from `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person` mp
+join `{{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement` m on mp.person_id = m.person_id
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement` mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
 where m.measurement_concept_id in (44783982, 40762636, 3038553)
 ),
 heart_rate as (
 select distinct mp.src_hpo_id, m.person_id
-from {{curation_project}}.{{ehr_ops_dataset}}._mapping_person mp
-join {{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement m on mp.person_id = m.person_id
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
+from `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person` mp
+join `{{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement` m on mp.person_id = m.person_id
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement` mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
 where m.measurement_concept_id in (3027018,37069660,3013901,3011020,3015006,3019657,3021659,36303780,40771524,3013786,3040891,
 40771525,40771526,3031207,3032300,36303943,3035288,3033692,3031069,36303602,3010413,3005326,3026028,3009881,21490736,3042292,3026867,
 3010183,3013100,3015625,3027998,3021545,40758551,3001376,3013883,36305351,36303801,3023766,42527141,1002621,40769110,1002782,
@@ -61,9 +61,9 @@ where m.measurement_concept_id in (3027018,37069660,3013901,3011020,3015006,3019
 ),
 blood_pressure_total as (
 select distinct mp.src_hpo_id, m.person_id
-from {{curation_project}}.{{ehr_ops_dataset}}._mapping_person mp
-join {{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement m on mp.person_id = m.person_id
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
+from `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person` mp
+join `{{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement` m on mp.person_id = m.person_id
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement` mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
 where m.measurement_concept_id in (3004249, 4152194, 3018586, 21490853, 3028737, 44789315,
 3005606, 3009395, 3035856, 4248525, 3018822, 36305059, 4232915, 4292062, 21490674, 3009435, 3012888, 4154790, 3034703,
 21490851, 44789316, 3017188, 3019962, 3013940, 4236281, 3018592, 36304326, 4248524, 4268883, 3027598, 3018336, 3012526, 21490675, 3017490, 21490680
@@ -71,17 +71,17 @@ where m.measurement_concept_id in (3004249, 4152194, 3018586, 21490853, 3028737,
 ),
 blood_pressure_systolic as (
 select distinct mp.src_hpo_id, m.person_id
-from {{curation_project}}.{{ehr_ops_dataset}}._mapping_person mp
-join {{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement m on mp.person_id = m.person_id
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
+from `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person` mp
+join `{{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement` m on mp.person_id = m.person_id
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement` mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
 where m.measurement_concept_id in (3004249, 4152194, 3018586, 21490853, 3028737, 44789315,
 3005606, 3009395, 3035856, 4248525, 3018822, 36305059, 4232915, 4292062, 21490674, 3009435)
 ),
 blood_pressure_diastolic as (
 select distinct mp.src_hpo_id, m.person_id
-from {{curation_project}}.{{ehr_ops_dataset}}._mapping_person mp
-join {{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement m on mp.person_id = m.person_id
-join {{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
+from `{{curation_project}}.{{ehr_ops_dataset}}._mapping_person` mp
+join `{{curation_project}}.{{ehr_ops_dataset}}.unioned_ehr_measurement` m on mp.person_id = m.person_id
+join `{{curation_project}}.{{ehr_ops_dataset}}._mapping_measurement` mm on mm.src_hpo_id = mp.src_hpo_id and mm.measurement_id = m.measurement_id
 where m.measurement_concept_id in (3012888, 4154790, 3034703,
 21490851, 44789316, 3017188, 3019962, 3013940, 4236281, 3018592, 36304326, 4248524, 4268883
 )
