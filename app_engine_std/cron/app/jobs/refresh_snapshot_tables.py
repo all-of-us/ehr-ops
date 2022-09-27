8380997f8ffcdedba7b32d7edd7eb05a9e75b174
jp3477 <jp3477@cumc.columbia.edu>
2022-09-27 12:41:58 -0400

    removed repeated snapshot_ts and fixed table name typo

--- app_engine_std/cron/app/jobs/refresh_snapshot_tables.py
+++ app_engine_std/cron/app/jobs/refresh_snapshot_tables.py
@@ -10,7 +10,6 @@ from aou_cloud.services.gcp_cloud_tasks import GCPCloudTask
 
 from ._base_job import BaseCronJob
 
-
 _logger = logging.getLogger('aou_cloud')
 
 
@@ -30,34 +29,24 @@ class RefreshSnapshotTablesJob(BaseCronJob):
         _logger.info('Creating Cloud Tasks to refresh snapshot tables')
 
         mv_tables = [
-            'mv_covid_mapping',
-            'mv_dc_1',
-            'mv_dc_2',
-            'mv_dc_3',
-            'mv_dc_4',
-            'mv_duplicates',
-            'mv_all_eligible_participants',
-            'mv_eligible_participants_ehr',
-            'mv_gc1_standard',
+            'mv_covid_mapping', 'mv_dc_1', 'mv_dc_2', 'mv_dc_3', 'mv_dc_4',
+            'mv_duplicates', 'mv_all_eligible_participants',
+            'mv_eligible_participants_ehr', 'mv_gc_1_standard',
             'mv_physical_meas',
-            'mv_table_counts_with_upload_timestamp',
-            'mv_unit_route_failure',
-            'mv_visit_id_failure',
-            'mv_nih_dc_1',
-            'mv_nih_dc_2',
-            'mv_nih_dc_3',
-            'mv_nih_dc_4',
-            'mv_nih_gc_1',
+            'mv_table_counts_with_upload_timestamp_for_hpo_sites',
+            'mv_unit_route_failure', 'mv_visit_id_failure', 'mv_nih_dc_1',
+            'mv_nih_dc_2', 'mv_nih_dc_3', 'mv_nih_dc_4', 'mv_nih_gc_1',
             'mv_in_person_participants'
         ]
 
         for table in mv_tables:
-            payload = {
-                'dataset': 'ehr_ops_metrics_staging',
-                'table': table
-            }
-            GCPCloudTask().execute('/task/refresh-snapshot-table', queue='cron-default', payload=payload,
+            payload = {'dataset': 'ehr_ops_metrics_staging', 'table': table}
+            GCPCloudTask().execute('/task/refresh-snapshot-table',
+                                   queue='cron-default',
+                                   payload=payload,
                                    project_id=self.gcp_env.project)
 
-        return JSONResponse(status_code=status.HTTP_200_OK,
-                            content=f'Job {self.gcp_env.project}.{self.job_name} has completed.')
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=f'Job {self.gcp_env.project}.{self.job_name} has completed.'
+        )
