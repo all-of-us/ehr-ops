8aa5ca039ce2e8154613cc21d0a638207d941a55
em3697 <101139133+em3697@users.noreply.github.com>
2023-02-08 18:12:56 -0500

    Em/zendesk integration (#63)
    * update yaml integration configuration
    
    * ignore zendesk utils
    
    * mock json response from zenpy
    
    * Update dashboard data source connections to postgresql (#60)
    
    * Update dashboard data source connections to postgresql
    
    * changed color scheme of DC rates sheet
    
    Co-authored-by: jp3477 <jp3477@cumc.columbia.edu>
    
    * Updated GCP logging, moved cron job and task base classes to services folder. (#61)
    
    * working on test_tag_intersection and setUp/tearDown methods
    
    * modularize ticketAutomation and work on create ticket test
    
    * test_create_ticket passing
    
    * remove unfinished test
    
    * Updated config.yml
    
    * add circleci environment variables
    
    * update utils file path
    
    * update test file creds
    
    * add hyphens to za commands
    
    * change config.yml formatting
    
    * add circleci script
    
    * try separating script from command in circleci
    
    * alter config.yml setup
    
    * make ticketAutomation module available
    
    * add requirements and ssh keys
    
    * try new realtive path to ticketAutomation for import
    
    * adjust level of import
    
    * import ticketAutomation
    
    * used absolute path for pythonpath
    
    * fixed stray hyphen
    
    * commented out pre-commit
    
    * changed call of super tear down
    
    * interchanged cls and self variables
    
    * exchanged hardcoded email for environment variable
    
    * changed teardownclass to teardown
    
    * removed classmethod decorator
    
    ---------
    
    Co-authored-by: Zoey Jiang <36078824+xj2193@users.noreply.github.com>
    Co-authored-by: jp3477 <jp3477@cumc.columbia.edu>
    Co-authored-by: Robert Abram <robert.m.abram.1@vumc.org>
    Co-authored-by: Jason Patterson <jp3477@columbia.edu>

---
+++ tests/zendesk_automation_test/test_tickets.py
@@ -0,0 +1,85 @@
+import os
+import unittest
+from zendesk_automation import ticketAutomation as ta
+import pandas as pd
+from zenpy import Zenpy
+
+
+class TestBasicInput(unittest.TestCase):
+    def tearDown(self) -> None:
+        for ticket in self.cleanup:
+            try:
+                self.zenpy_client.tickets.delete(ticket)
+            except Zenpy.ApiException:
+                print()
+
+    def setUp(self) -> None:
+
+        self.cleanup = []
+
+        CREDENTIALS = {
+            "email": os.environ['EMAIL'],
+            "token": os.environ['ZENPY_TOKEN'],
+            "subdomain": os.environ['SUBDOMAIN']
+        }
+        self.zenpy_client = Zenpy(**CREDENTIALS)
+
+    def test_create_ticket(self):
+        pd.set_option('display.max_columns', None)
+        contact_data = [['Test', 'test_create_ticket', os.environ['EMAIL']]]
+        site_contact_df = pd.DataFrame(
+            contact_data,
+            columns=[
+                'Site Name', 'hpo_id',
+                'Point of Contact (mostly data stewards)\n*Use semi-colons to separate the email addresses*'
+            ])
+        submission_data = [[
+            'Test', 'Elise', os.environ['EMAIL'], 'test_create_ticket'
+        ]]
+        submission_tracking_df = pd.DataFrame(
+            submission_data,
+            columns=['Site Name', 'contact', 'contact_email', 'hpo_id'])
+
+        src_hpo_id = 'test_create_ticket'
+        hpo_name = 'Test'
+        metric = 'gc1'
+        scores = {
+            f'_{metric}': 0.97510765,
+            'condition': 0.940730566,
+            'drug': 0.825231502,
+            'measurement': 0,
+            'observation': 1,
+            'procedure': 0.89552811,
+            'visit': 0.862934988
+        }
+        table_name = 'condition'
+        tag_list = [
+            'test_create_ticket', metric, table_name, 'auto-test-tickets'
+        ]
+        ticket_obj = ta.create_ticket(self.zenpy_client,
+                                      submission_tracking_df, src_hpo_id,
+                                      site_contact_df, scores, metric,
+                                      table_name, tag_list, hpo_name)
+        ticket_body = list(
+            self.zenpy_client.tickets.comments(ticket=ticket_obj.id))[0].body
+        ticket_subject = ticket_obj.subject
+        expected_subject = f"GC1 Condition Table Data Quality Issue Flagged"
+        expected_body = f'''Hi Test, 
+        
+        In your latest submission, your GC-1 rate was 0.94 for the Condition Table, which is below our acceptance threshold. GC-1 measures conformance to OMOP standard concepts and is a priority for data quality. 
+        
+        There is additional information linked here, along with SQL queries to help you identify the issue: https://aou-ehr-ops.zendesk.com/hc/en-us/articles/1500012365822-NIH-Grant-Award-Metrics-
+
+        You can access our EHR Ops dashboard here: https://drc.aouanalytics.org/#/views/EHROpsGeneralDataQualityDashboard/Home
+
+        Please fix this data quality issue and resubmit. 
+
+        Thanks, 
+        EHR Ops Team'''
+        self.cleanup.append(ticket_obj)
+        self.assertTrue(ticket_subject == expected_subject)
+        self.assertMultiLineEqual(ticket_body, expected_body)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- zendesk_automation/ticketAutomation.py
+++ zendesk_automation/ticketAutomation.py
@@ -1,19 +1,16 @@
 import os
-from unittest import case
-import utils
 import pandas as pd
 from zenpy import Zenpy
-from impersonation_tools import get_impersonation_credentials
-from zenpy.lib.api_objects import Ticket, User, Comment
+from zenpy.lib.api_objects import Ticket, Comment
 from google.cloud import bigquery
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
 # Zenpy credentials
-credentials = {
-    "email": utils.EMAIL,
-    "token": utils.ZENPY_TOKEN,
-    "subdomain": utils.SUBDOMAIN
+CREDENTIALS = {
+    "email": os.environ['EMAIL'],
+    "token": os.environ['ZENPY_TOKEN'],
+    "subdomain": os.environ['SUBDOMAIN']
 }
 
 # Google scopes
@@ -23,26 +20,34 @@ DEFAULT_SCOPES = [
     'https://www.googleapis.com/auth/drive'
 ]
 
-def tag_intersection(status_list, tag_list):
+COMMENT_BODY = '''
+This ticket has not been resolved. Please make the necessary changes or reach out to our team if you have questions or concerns.
+
+Thanks, 
+EHR Ops Team
+'''
+
+# Set Google Application Credentials to key file
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"] = "./zendesk_automation/GOOGLE_APP_CREDS.txt"
+
+
+def tag_intersection(zenpy_client, status_list, tag_list):
     # create set of ticket ids that will be intersected on each search
     ticket_ids = {}
     for index, tag in enumerate(tag_list):
-        search = zenpy_client.search(status=status_list, type="ticket", tags=tag)
+        search = zenpy_client.search(status=status_list,
+                                     type="ticket",
+                                     tags=tag)
 
         if index == 0:
             ticket_ids = set(search)
-        
+
         else:
             ticket_ids.intersection(search)
 
     return ticket_ids
 
-comment_body = '''
-This ticket has not been resolved. Please make the necessary changes or reach out to our team if you have questions or concerns.
-
-Thanks, 
-EHR Ops Team
-'''     
 
 def get_ticket_body(table_name, metric, metric_value, hpo_name):
     # gc1, pysical measurement, covid mapping, measurement integration
@@ -63,10 +68,10 @@ def get_ticket_body(table_name, metric, metric_value, hpo_name):
         Thanks, 
         EHR Ops Team 
         '''
-    
+
     elif metric == 'physical_measurement':
         ticket_body = f'''
-        Hi {hpo_name}, 
+        Hi {hpo_name},
         
         In your latest submission, the physical measurements you submitted were either marked as missing or fell below our threshold. 
         This is a high priority data quality metric: particularly height, weight, blood pressure, and heart rate. 
@@ -96,126 +101,221 @@ def get_ticket_body(table_name, metric, metric_value, hpo_name):
 
     return ticket_body
 
-# Create google sheets credentials
-key_file_location = utils.KEYFILE_LOC
-scopes=['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
-creds = service_account.Credentials.from_service_account_file(key_file_location, scopes=scopes)
 
-# Authenticate and construct service.
-service = build('sheets', 'v4', credentials = creds)
-
-# Get Site Contact data
-# sheet = service.spreadsheets()
-# site_contact_result = sheet.values().get(spreadsheetId='1aS9etPRp1pHpms-uL1mnwoqK7B6uuWfXgU3MabbCyFE',
-#                             range='Site Contacts').execute()
-# site_contact_values = site_contact_result.get('values')
-# site_contact_df = pd.DataFrame(site_contact_values[1:], columns=site_contact_values[0])
-
-# Get TEST Site Contact data
-sheet = service.spreadsheets()
-site_contact_result = sheet.values().get(spreadsheetId=utils.SITE_CONTACT_ID,
-                            range='Site Contacts').execute()
-site_contact_values = site_contact_result.get('values')
-site_contact_df = pd.DataFrame(site_contact_values[1:], columns=site_contact_values[0])
-
-# Get Submission Tracking data
-submission_tracking_result = sheet.values().get(spreadsheetId=utils.SUBMISSION_TRACK_ID,
-                            range='Mapping').execute()
-submission_tracking_values = submission_tracking_result.get('values')
-submission_tracking_df = pd.DataFrame(submission_tracking_values[1:], columns=submission_tracking_values[0])
+def comment_update(zenpy_client, search, comment_body):
+    for ticket_obj in search:
+        ticket_id = ticket_obj.id
+        ticket = zenpy_client.tickets(id=ticket_id)
+        print(f'Commenting on Zendesk ticket with ID {ticket_id}...')
+        ticket.comment = Comment(body=comment_body, public=False)
+        zenpy_client.tickets.update(ticket)
+    return search
 
 
+def create_ticket(zenpy_client, submission_tracking_df, src_hpo_id,
+                  site_contact_df, scores, metric, table_name, tag_list,
+                  hpo_name):
+    hpo_row = submission_tracking_df.loc[submission_tracking_df['hpo_id'] ==
+                                         src_hpo_id]
+    # assignee_email = hpo_row['contact_email']
+    # assignee = zenpy_client.search(type='user', email=assignee_email)
+    # assignee_id = list(assignee)[0].id
+    assignee_email = os.environ['EMAIL']
+    assignee = zenpy_client.search(type='user', email=assignee_email)
+    assignee_id = list(assignee)[0].id
+    # requester = zenpy_client.search(type='user', email='noreply@researchallofus.org')
+    # requester_id = list(requester)[0].id
+    requester = zenpy_client.search(type='user', email=os.environ['EMAIL'])
+    requester_id = list(requester)[0].id
+    hpo_name = list(site_contact_df[site_contact_df['hpo_id'] == src_hpo_id]
+                    ['Site Name'])[0]
+    if table_name == '':
+        metric_value = scores[f'_{metric}']
+        table_phrase = table_name
+    else:
+        metric_value = scores[table_name]
+        table_name = table_name.title() + " Table"
+        table_phrase = " for the " + table_name
+    metric_upper = metric.upper()
+    ticket_descr = get_ticket_body(table_phrase, metric_upper,
+                                   round(metric_value, 2), hpo_name)
+    print('Creating Zendesk ticket...')
+    ticket_audit = zenpy_client.tickets.create(
+        Ticket(
+            requester_id=requester_id,
+            assignee_id=assignee_id,
+            subject=f"{metric_upper} {table_name} Data Quality Issue Flagged",
+            description=ticket_descr,
+            tags=tag_list))
+    return ticket_audit.ticket
 
-# Set Google Application Credentials to key file
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= utils.GOOGLE_APP_CREDS
 
-zenpy_client = Zenpy(**credentials)
+def get_hpo_list(filled_scores_df, metric, threshold):
+    hpo_list = filled_scores_df[
+        (filled_scores_df[f'condition_{metric}'] < threshold) |
+        (filled_scores_df[f'drug_{metric}'] < threshold) |
+        (filled_scores_df[f'measurement_{metric}'] < threshold) |
+        (filled_scores_df[f'observation_{metric}'] < threshold) |
+        (filled_scores_df[f'procedure_{metric}'] < threshold) |
+        (filled_scores_df[f'visit_{metric}'] < threshold) |
+        (filled_scores_df[f'_{metric}'] < threshold)]
+    return hpo_list
 
-# Create bigquery client
-bigquery_client = bigquery.Client()
 
-# get list of hpo_ids
-hpo_list = site_contact_df.loc[site_contact_df['hpo_id'] != '']['hpo_id'].to_list()
+def get_table_metric(row, metric):
+    metric_by_table = {
+        f'_{metric}': row[f'_{metric}'],
+        'condition': row[f'condition_{metric}'],
+        'drug': row[f'drug_{metric}'],
+        'measurement': row[f'measurement_{metric}'],
+        'observation': row[f'observation_{metric}'],
+        'procedure': row[f'procedure_{metric}'],
+        'visit': row[f'visit_{metric}']
+    }
+    return metric_by_table
 
-# get symmetric difference between hpo_list and list to exclude
-exclude = ['vcu', 'wash_u_stl']
-included_hpos = set(hpo_list) ^ set(exclude)
 
-metrics = ['gc1']
+def get_table_name(name, metric):
+    return name.split(f'_{metric}')[0]
+
+
+def evaluate_metrics(zenpy_client, scores, metric, src_hpo_id,
+                     submission_tracking_df, site_contact_df):
+    for name, score in scores.items():
+        # If the score is low (whole row is returned for the hpo_id) begin search & comment or create ticket workflow
+        if score < 0.9:
+            # search for pending and open tickets with GC-1 and hpo_id
+            table_name = get_table_name(name, metric)
+            if len(table_name) < 1 or metric != 'gc1':
+                tag_list = [src_hpo_id, metric, "auto-test-tickets"]
+            else:
+                tag_list = [
+                    src_hpo_id, table_name, metric, "auto-test-tickets"
+                ]
+
+            ticket_status = ['open', 'pending']
+            search = tag_intersection(zenpy_client, ticket_status, tag_list)
+
+            # TODO: Decide what to do if multiple tickets are returned by the search
+            # If the search does return a ticket then add a comment to the existing ticket
+            if len(search) > 0:
+                comment_update(zenpy_client, search, COMMENT_BODY)
+
+            # If the search does not return a ticket then create a new ticket
+            else:
+                create_ticket(zenpy_client, submission_tracking_df, src_hpo_id,
+                              site_contact_df, scores, metric, table_name,
+                              tag_list)
+
+    return
+
+
+def zenpy_obj_to_json(filename, request):
+    with open(f"{filename}.json", "w") as outfile:
+        outfile.write('[')
+        for i, each in enumerate(request):
+            outfile.write(each.to_json())
+            if i < len(request) - 1:
+                outfile.write(',')
+        outfile.write(']')
+    return
+
+
+def get_sheets():
+    # Create google sheets credentials
+    key_file_location = "./zendesk_automation/KEYFILE_LOC.txt"
+    scopes = [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/spreadsheets'
+    ]
+    creds = service_account.Credentials.from_service_account_file(
+        key_file_location, scopes=scopes)
+
+    # Authenticate and construct service.
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Get Site Contact data
+    # sheet = service.spreadsheets()
+    # site_contact_result = sheet.values().get(spreadsheetId='1aS9etPRp1pHpms-uL1mnwoqK7B6uuWfXgU3MabbCyFE',
+    #                             range='Site Contacts').execute()
+    # site_contact_values = site_contact_result.get('values')
+    # site_contact_df = pd.DataFrame(site_contact_values[1:], columns=site_contact_values[0])
+
+    # Get TEST Site Contact data
+    sheet = service.spreadsheets()
+    site_contact_result = sheet.values().get(
+        spreadsheetId=os.getenv.SITE_CONTACT_ID,
+        range='Site Contacts').execute()
+    site_contact_values = site_contact_result.get('values')
+    site_contact_df = pd.DataFrame(site_contact_values[1:],
+                                   columns=site_contact_values[0])
+
+    # Get Submission Tracking data
+    submission_tracking_result = sheet.values().get(
+        spreadsheetId=os.getenv.SUBMISSION_TRACK_ID,
+        range='Mapping').execute()
+    submission_tracking_values = submission_tracking_result.get('values')
+    submission_tracking_df = pd.DataFrame(
+        submission_tracking_values[1:], columns=submission_tracking_values[0])
+
+    return [site_contact_df, submission_tracking_df]
+
 
 # CALCULATE EACH METRIC
-# TODO: add loop going through each metric where the query file is a variable 
-for metric in metrics:
+def ticket_automation():
 
-    # GC1 query/calculation for included hpos
-    job_config = bigquery.QueryJobConfig(
-        query_parameters=[
-            bigquery.ArrayQueryParameter("included_hpos", "STRING", included_hpos)
-        ]
-    )
-    f = open(f'{metric}.sql')
-    QUERY = f.read()
-    # API request
-    query_job = bigquery_client.query(QUERY, job_config=job_config)
-    scores_df = query_job.result().to_dataframe()
-    filled_scores_df = scores_df.fillna(0)
+    site_contact_df, submission_tracking_df = get_sheets()
 
-    # If metrics are returned
-    if filled_scores_df.shape[0] > 0:
-        # TODO: should I just remove the metric name from the column names so the variables can be generic for each metric?
-        # Find all hpos with scores below the threshold
-        low_scores_df = filled_scores_df[(filled_scores_df[f'condition_{metric}'] < 0.9) | (filled_scores_df[f'drug_{metric}'] < 0.9) | (filled_scores_df[f'measurement_{metric}'] < 0.9) | (filled_scores_df[f'observation_{metric}'] < 0.9) | (filled_scores_df[f'procedure_{metric}'] < 0.9) | (filled_scores_df[f'visit_{metric}'] < 0.9) | (filled_scores_df[f'_{metric}'] < 0.9)]
-        
-        # For each hpo with a low score evaluate which table has the low metric
-        for index, row in low_scores_df.iterrows():
-            src_hpo_id = row['src_hpo_id']
+    zenpy_client = Zenpy(**CREDENTIALS)
 
-            # Dictionary of current hpo's tables and respective calculation
-            scores = {f'_{metric}': row[f'_{metric}'], 'condition': row[f'condition_{metric}'], 'drug': row[f'drug_{metric}'], 'measurement': row[f'measurement_{metric}'], 'observation': row[f'observation_{metric}'], 'procedure': row[f'procedure_{metric}'], 'visit': row[f'visit_{metric}']}
-            
-            for name, score in scores.items():
-                
-                # If the score is low (whole row is returned for the hpo_id) begin search & comment or create ticket workflow
-                if score < 0.9:
-                    # search for pending and open tickets with GC-1 and hpo_id
-                    table_name = name.split(f'_{metric}')[0]
-                    if len(table_name) < 1 or metric != 'gc1':
-                        tag_list = [src_hpo_id, metric, "auto-test-tickets"]
-                    else:
-                        tag_list = [src_hpo_id, table_name, metric, "auto-test-tickets"]
-                    
-                    ticket_status = ['open', 'pending']
-                    search = tag_intersection(ticket_status, tag_list)
+    request = zenpy_client.tickets(tags=["auto-test-tickets"])
+    zenpy_obj_to_json("mock", request)
 
-                    # TODO: Decide what to do if multiple tickets are returned by the search
-                    # If the search does return a ticket then add a comment to the existing ticket
-                    if len(search) > 0:
-                        for ticket_obj in search:
-                            ticket_id = ticket_obj.id
-                            ticket = zenpy_client.tickets(id=ticket_id)
-                            print(f'Commenting on Zendesk ticket with ID {ticket_id}...')
-                            ticket.comment = Comment(body=comment_body, public=False)
-                            zenpy_client.tickets.update(ticket)
+    # Create bigquery client
+    bigquery_client = bigquery.Client()
 
-                    # If the search does not return a ticket then create a new ticket
-                    else:
-                        hpo_row=submission_tracking_df.loc[submission_tracking_df['hpo_id'] == src_hpo_id]
-                        assignee_email = hpo_row['contact_email']
-                        assignee = zenpy_client.search(type='user', email=assignee_email)
-                        assignee_id = list(assignee)[0].id
-                        requester = zenpy_client.search(type='user', email='noreply@researchallofus.org')
-                        requester_id = list(requester)[0].id
-                        hpo_name = list(site_contact_df[site_contact_df['hpo_id'] == src_hpo_id]['Site Name'])[0]
-                        if table_name == '':
-                            metric_value = scores[f'_{metric}']
-                            table_phrase = table_name
-                        else:
-                            metric_value = scores[table_name]
-                            table_name = table_name.title() + " Table"
-                            table_phrase = " for the " + table_name + " table"
-                        metric_upper = metric.upper()
-                        ticket_descr = get_ticket_body(table_phrase, metric_upper, round(metric_value, 2), hpo_name)
-                        print('Creating Zendesk ticket...')
-                        zenpy_client.tickets.create(Ticket(requester_id=requester_id, assignee_id=assignee_id, 
-                                                            subject=f"{metric_upper} {table_name} Data Quality Issue Flagged", 
-                                                            description=ticket_descr, tags=tag_list))
+    # get list of hpo_ids
+    hpo_list = site_contact_df.loc[
+        site_contact_df['hpo_id'] != '']['hpo_id'].to_list()
+
+    # get symmetric difference between hpo_list and list to exclude
+    exclude = ['vcu', 'wash_u_stl']
+    included_hpos = set(hpo_list) ^ set(exclude)
+
+    metrics = ['gc1']
+
+    for metric in metrics:
+
+        # GC1 query/calculation for included hpos
+        job_config = bigquery.QueryJobConfig(query_parameters=[
+            bigquery.ArrayQueryParameter("included_hpos", "STRING",
+                                         included_hpos)
+        ])
+        f = open(f'{metric}.sql')
+        QUERY = f.read()
+        # API request
+        query_job = bigquery_client.query(QUERY, job_config=job_config)
+        scores_df = query_job.result().to_dataframe()
+        filled_scores_df = scores_df.fillna(0)
+
+        # If metrics are returned
+        if filled_scores_df.shape[0] > 0:
+            # TODO: should I just remove the metric name from the column names so the variables can be generic for each metric?
+            # Find all hpos with scores below the threshold
+            low_scores_df = get_hpo_list(filled_scores_df,
+                                         metric,
+                                         threshold=0.9)
+
+            # For each hpo with a low score evaluate which table has the low metric
+            for _, row in low_scores_df.iterrows():
+                src_hpo_id = row['src_hpo_id']
+
+                # Dictionary of current hpo's tables and respective calculation
+                scores = get_table_metric(row, metric)
+
+                evaluate_metrics(zenpy_client, scores, metric, src_hpo_id,
+                                 submission_tracking_df, site_contact_df)
+
+
+if __name__ == '__main__':
+    ticket_automation()
