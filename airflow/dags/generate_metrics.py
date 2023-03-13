44003c35fdac446437292531800516b8cb73c3c3
jp3477 <jp3477@cumc.columbia.edu>
2023-03-13 16:53:46 -0400

    added dependency order between airflow tasks

--- airflow/dags/generate_metrics.py
+++ airflow/dags/generate_metrics.py
@@ -6,98 +6,113 @@ import networkx as nx
 import json
 from pathlib import Path
 import re
-from google.cloud import bigquery as python_client_bigquery
+# from google.cloud import bigquery as python_client_bigquery
 
-DEPENDENCIES_FILE = Path(__file__) / 'dependencies.json'
+DEPENDENCIES_FILE = Path(__file__).parent / 'dependencies.json'
 
 
 def parse_dependencies():
     with open(DEPENDENCIES_FILE, 'r') as f:
         dependencies = json.load(f)
 
-    g = nx.Graph()
+    g = nx.DiGraph()
 
     for task_id, details in dependencies.items():
         if 'depends_on' in details:
             for dependency in details['depends_on']:
-                g.add_edge(task_id, dependency)
+                g.add_edge(dependency, task_id)
 
     return g
 
 
 def order_tasks(task_dict):
     dependency_graph = parse_dependencies()
-    assert nx.is_directed_acyclic_graph(dependency_graph)
+    # assert nx.is_directed_acyclic_graph(dependency_graph)
 
-    for task_id, prev_task_id in dependency_graph.edges:
-        if task_id in task_dict and prev_task_id in task_dict:
-            task_dict[prev_task_id] >> task_dict[task_id]
+    for dependency, task_id in dependency_graph.edges:
+        if task_id in task_dict and dependency in task_dict:
+            task_dict[dependency] >> task_dict[task_id]
 
 
 @dag(schedule_interval=None,
      start_date=pendulum.datetime(2023, 1, 1, tz="UTC"))
 def generate_metrics():
-    CURATION_PROJECT_ID = 'aou-res-curation-prod'
-    EHR_OPS_PROJECT_ID = 'aou-ehr-ops-curation-test'
-    EHR_OPS_RESOURCES_DATASET_ID = 'ehr_ops_resources'
-    EHR_OPS_METRICS_STAGING_DATASET_ID = 'ehr_ops_metrics_staging'
-    EHR_OPS_DATASET_ID = 'ehr_ops'
+    EHR_OPS_PROJECT_ID = Variable.get('ehr_ops_project_id')
+    EHR_OPS_RESOURCES_DATASET_ID = Variable.get('ehr_ops_resources_dataset_id')
+    EHR_OPS_METRICS_STAGING_DATASET_ID = Variable.get(
+        'ehr_ops_metrics_staging_dataset_id')
 
-    # Pull list of metric views
-    @task(task_id=f'retrieve_metric_view_list')
-    def retrieve_metric_view_list():
-        hook = bigquery.BigQueryHook(gcp_conn_id='aou_ehr_ops_curation_test',
-                                     use_legacy_sql=False)
+    # Get list of metric views from variables
+    view_list = Variable.get("bq_view_list", deserialize_json=True)
+    curation_project_id = Variable.get(curation_project_id)
 
-        client = python_client_bigquery.Client(
-            project=hook._get_field('project'),
-            credentials=hook._get_credentials())
+    task_dict = {}
+    for view in view_list:
+        view_id = view['view_name']
+        task_id = f'build_metrics_{view_id}'
 
-        sql = """
+        @task(task_id=task_id)
+        def build_metrics():
+            materialized_view_id = f"mv_{re.match('v_(.+)', view_id)[1]}"
+
+            hook = bigquery.BigQueryHook(
+                gcp_conn_id='aou_ehr_ops_curation_test', use_legacy_sql=False)
+
+            metrics_query_tmpl = """
             SELECT
-                table_catalog, table_schema, table_name
-            FROM `{project_id}.{dataset_id}.INFORMATION_SCHEMA.TABLES`
-            WHERE table_type='VIEW'
-        """.format(project_id=EHR_OPS_PROJECT_ID,
-                   dataset_id=EHR_OPS_RESOURCES_DATASET_ID)
+                *
+            FROM `{project_id}.{dataset_id}.{view_id}`
+            """
 
-        results = client.query(sql)
-        view_names = [row["table_name"] for row in results]
+            metrics_query = metrics_query_tmpl.format(
+                project_id=EHR_OPS_PROJECT_ID,
+                dataset_id=EHR_OPS_RESOURCES_DATASET_ID,
+                view_id=view_id)
 
-        return view_names
+            destination_dataset_table = f'{EHR_OPS_METRICS_STAGING_DATASET_ID}.{materialized_view_id}'
+            hook.run_query(metrics_query,
+                           destination_dataset_table=destination_dataset_table,
+                           write_disposition='WRITE_TRUNCATE')
+
+        task_dict[task_id] = build_metrics()
+
+    order_tasks(task_dict)
+
+    # # Pull list of metric views
+    # @task(task_id=f'retrieve_metric_view_list')
+    # def retrieve_metric_view_list():
+    #     hook = bigquery.BigQueryHook(gcp_conn_id='aou_ehr_ops_curation_test',
+    #                                  use_legacy_sql=False)
+
+    #     client = python_client_bigquery.Client(
+    #         project=hook._get_field('project'),
+    #         credentials=hook._get_credentials())
+
+    #     sql = """
+    #         SELECT
+    #             table_catalog, table_schema, table_name
+    #         FROM `{project_id}.{dataset_id}.INFORMATION_SCHEMA.TABLES`
+    #         WHERE table_type='VIEW'
+    #     """.format(project_id=EHR_OPS_PROJECT_ID,
+    #                dataset_id=EHR_OPS_RESOURCES_DATASET_ID)
+
+    #     results = client.query(sql)
+    #     view_names = [row["table_name"] for row in results]
+
+    #     return view_names
 
     # Decide dependency order of views to execute
 
     # Execute each view in `ehr_ops_resources` and save results as tables
 
-    @task
-    def build_metrics(view_id):
-        materialized_view_id = f"mv_{re.match('v_(.+)', view_id)[1]}"
-
-        hook = bigquery.BigQueryHook(gcp_conn_id='aou_ehr_ops_curation_test',
-                                     use_legacy_sql=False)
-
-        metrics_query_tmpl = """
-        SELECT
-            *
-        FROM `{project_id}.{dataset_id}.{view_id}`
-        """
-
-        metrics_query = metrics_query_tmpl.format(
-            project_id=EHR_OPS_PROJECT_ID,
-            dataset_id=EHR_OPS_RESOURCES_DATASET_ID,
-            view_id=view_id)
-
-        destination_dataset_table = f'{EHR_OPS_PROJECT_ID}.{EHR_OPS_METRICS_STAGING_DATASET_ID}.{materialized_view_id}'
-        hook.run_query(metrics_query,
-                       destination_dataset_table=destination_dataset_table,
-                       write_disposition='WRITE_TRUNCATE')
-
-    build_metrics.expand(view_id=retrieve_metric_view_list())
+    # build_metrics.expand(view_id=retrieve_metric_view_list())
 
     # Message success or failure to `metric-load-completed` and `metric-load-failed` PubSub topics
 
-    # Snapshot tables
+    # Snapshot tables com
 
 
 generate_metrics = generate_metrics()
+
+if __name__ == '__main__':
+    dag.test()
---
+++ airflow/dags/refresh_view_list.py
@@ -0,0 +1,48 @@
+from airflow.decorators import dag, task
+from airflow.providers.google.cloud.hooks import bigquery
+from airflow.models import Variable
+import pendulum
+import networkx as nx
+import json
+from pathlib import Path
+import re
+from google.cloud import bigquery as python_client_bigquery
+
+
+@dag(schedule_interval=None,
+     start_date=pendulum.datetime(2023, 1, 1, tz="UTC"))
+def refresh_view_list():
+
+    EHR_OPS_PROJECT_ID = Variable.get('ehr_ops_project_id')
+    EHR_OPS_RESOURCES_DATASET_ID = Variable.get('ehr_ops_resources_dataset_id')
+
+    @task(task_id=f'retrieve_metric_view_list')
+    def retrieve_metric_view_list():
+        hook = bigquery.BigQueryHook(gcp_conn_id='aou_ehr_ops_curation_test',
+                                     use_legacy_sql=False)
+
+        client = python_client_bigquery.Client(
+            project=hook._get_field('project'),
+            credentials=hook._get_credentials())
+
+        sql = """
+            SELECT
+                table_catalog project_id, table_schema dataset_id, table_name view_name
+            FROM `{project_id}.{dataset_id}.INFORMATION_SCHEMA.TABLES`
+            WHERE table_type='VIEW'
+        """.format(project_id=EHR_OPS_PROJECT_ID,
+                   dataset_id=EHR_OPS_RESOURCES_DATASET_ID)
+
+        results = client.query(sql)
+        views = [dict(row) for row in results]
+
+        return views
+
+    @task(task_id='update_view_list_variable')
+    def update_view_list_variable(views):
+        Variable.set("bq_view_list", views, serialize_json=True)
+
+    update_view_list_variable(retrieve_metric_view_list())
+
+
+refresh_view_list()
