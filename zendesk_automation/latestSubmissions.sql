7a7f5cadff5878a85b1fecde3b086babe1c692e2
em3697 <101139133+em3697@users.noreply.github.com>
2023-04-11 11:53:45 -0400

    Update zendesk_automation/latestSubmissions.sql
    Co-authored-by: Jason Patterson <jp3477@cumc.columbia.edu>

--- zendesk_automation/latestSubmissions.sql
+++ zendesk_automation/latestSubmissions.sql
@@ -1,4 +1,4 @@
 SELECT DISTINCT
 hpo_id
-FROM `aou-res-curation-prod.operations_analytics..table_counts_with_upload_timestamp_for_hpo_sites`
+FROM `{{curation_project}}.{{operations_analytics_dataset}}.table_counts_with_upload_timestamp_for_hpo_sites`
 WHERE TIMESTAMP_DIFF(CURRENT_TIMESTAMP(), person_upload_time, HOUR) <= 24
