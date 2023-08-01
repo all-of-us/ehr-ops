1abf7cf1a55f495b847f70f39d3391af18a2d7a5
em3697 <em3697@cumc.columbia.edu>
2023-08-01 16:33:08 -0400

    created test ticket for EHR consent issues

--- zendesk_automation/latestSubmissions.sql
+++ zendesk_automation/latestSubmissions.sql
@@ -1,4 +1,4 @@
 SELECT DISTINCT
 hpo_id
-FROM `{{curation_project}}.{{operations_analytics_dataset}}.table_counts_with_upload_timestamp_for_hpo_sites`
+FROM `aou-ehr-ops-curation-prod.ehr_ops_metrics_staging.mv_table_counts_with_upload_timestamp_for_hpo_sites`
 WHERE TIMESTAMP_DIFF(CURRENT_TIMESTAMP(), person_upload_time, HOUR) <= 24
