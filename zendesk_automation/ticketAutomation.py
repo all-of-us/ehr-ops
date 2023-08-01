bd274e8e0b4ecd362dc62d8d2aa30ff88542f75e
em3697 <em3697@cumc.columbia.edu>
2023-08-01 16:33:33 -0400

    add comment for tester id

--- zendesk_automation/ticketAutomation.py
+++ zendesk_automation/ticketAutomation.py
@@ -190,6 +190,8 @@ def ticket_update(ticket_action, zenpy_client, submission_tracking_df, src_hpo_i
     
 
     elif ticket_action == 'ticket':
+        #tester = zenpy_client.search(type='user', email='em3697@cumc.columbia.edu')
+        #tester_id = list(tester)[0].id
         print('Creating Zendesk ticket...')
         ticket_audit = zenpy_client.tickets.create(
             Ticket(
