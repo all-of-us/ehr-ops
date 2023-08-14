8f6273fde5760ea0eaab101506ca695544103fb7
em3697 <em3697@cumc.columbia.edu>
2023-08-14 16:45:50 -0400

    template table names

--- zendesk_automation/ehr_consent_status.sql
+++ zendesk_automation/ehr_consent_status.sql
@@ -1,6 +1,6 @@
 Select participant_id, ORGANIZATION, withdrawal_flag, ehr_consent_yes_flag, LOWER(mapping.HPO_ID) as HPO_ID 
-from `aou-ehr-ops-curation-prod.ehr_ops_metrics_staging.mv_ehr_rdr_participant` mv_ehr_rdr_participant 
-join `aou-ehr-ops-curation-prod.ehr_ops_metrics_staging.mv_org_hpo_mapping` mapping
+from `{{ehr_ops_project}}.{{ehr_ops_staging_dataset}}.mv_ehr_rdr_participant` mv_ehr_rdr_participant 
+join `{{ehr_ops_project}}.{{ehr_ops_staging_dataset}}.mv_org_hpo_mapping` mapping
 ON mapping.Org_ID = mv_ehr_rdr_participant.ORGANIZATION
 where (ehr_consent_yes_flag !=1 or TIMESTAMP(withdrawal_time) < mv_ehr_rdr_participant.person_upload_time) 
 and (person_flag = 1 or condition_flag = 1 or device_flag = 1 or death_flag = 1 or drug_flag = 1 or measurement_flag = 1 or procedure_flag = 1 or observation_flag = 1 or visit_flag = 1 or speciment_flag = 1)
