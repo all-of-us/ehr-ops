667a82a99d81231fd3419a5925cbb32aaeebc713
Xinzhuo Jiang <xj2193@cumc.columbia.edu>
2024-03-12 10:03:20 -0400

    EHR-966 Manually create linkage for COSSMA

--- dashboard_metrics/EHR Ops Dashboard/sql/ehr_metrics/general_metrics/v_org_hpo_mapping.sql
+++ dashboard_metrics/EHR Ops Dashboard/sql/ehr_metrics/general_metrics/v_org_hpo_mapping.sql
@@ -1,34 +1,48 @@
 -- This query returns the org_id linkage between CDR and PDR
 WITH
   bucket_create AS (
-  SELECT
-    resource.labels.bucket_name,
-    MAX(timestamp) AS bucket_created_timestamp
-  FROM
-    `{{rdr_project}}.{{gcs_logging}}.cloudaudit_googleapis_com_activity_20*`
-  WHERE
-    resource.type = 'gcs_bucket'
-    AND protopayload_auditlog.methodName = 'storage.buckets.create'
-    AND resource.labels.bucket_name LIKE 'aou%'
-  GROUP BY
-    1)
+  -- SELECT
+  --   resource.labels.bucket_name,
+  --   MAX(timestamp) AS bucket_created_timestamp
+  -- FROM
+  --   `{{rdr_project}}.GcsBucketLogging.cloudaudit_googleapis_com_activity_20*`
+  -- WHERE
+  --   resource.type = 'gcs_bucket'
+  --   AND protopayload_auditlog.methodName = 'storage.buckets.create'
+  --   AND resource.labels.bucket_name LIKE 'aou%'
+  -- GROUP BY
+  --   1
+  SELECT * FROM `{{ehr_ops_project}}.{{ehr_ops_resources_dataset}}.hpo_onboarding`),
+
+updated_hpo_site_id_mappings AS
+(
 SELECT
-  external_id,
-  m.Org_ID,
-  m.HPO_ID,
-  m.Display_Order,
-  b.bucket_created_timestamp AS On_Boarding_Time
+  CASE WHEN Org_ID = 'COSSMA_PR' THEN 'COSSMA_63K3' ---build the linkage between RDR and EHR for COSSMA organization
+  ELSE Org_ID
+  END AS Org_ID,
+  HPO_ID,
+  Display_Order
 FROM
+  `{{curation_project}}.{{lookup_dataset}}.hpo_site_id_mappings`
+ )
+
+SELECT coalesce(o.external_id, m.Org_ID) as external_id,
+m.Org_ID,
+m.HPO_ID,
+m.Display_Order,
+b.bucket_created_timestamp AS On_Boarding_Time
+FROM updated_hpo_site_id_mappings m
+LEFT JOIN
   `{{pdr_project}}.{{rdr_ops_dataset}}.v_organization` o
-JOIN
-  `{{curation_project}}.{{lookup_dataset}}.hpo_site_id_mappings` m
 ON
   o.external_id = m.Org_ID
-JOIN
+LEFT JOIN
   `{{curation_project}}.{{lookup_dataset}}.hpo_id_bucket_name` h
 ON
   m.HPO_ID = h.hpo_id
-JOIN
+LEFT JOIN
   bucket_create b
 USING
   (bucket_name)
+WHERE m.Org_ID NOT LIKE '%FHIR%'
+AND b.bucket_created_timestamp IS NOT NULL
