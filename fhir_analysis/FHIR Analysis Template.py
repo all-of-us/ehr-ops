52f42608a77879d41ac341dfe1d44a0c715c3738
jp3477 <jp3477@cumc.columbia.edu>
2024-03-12 18:05:33 -0400

    converted ipynbs to jupytext

---
+++ fhir_analysis/FHIR Analysis Template.py
@@ -0,0 +1,70 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Load Packages
+
+# +
+# Add any needed packages
+
+from google.cloud import bigquery
+# -
+
+# # Setup Connection
+
+# +
+# Fill in identifiers
+
+EHR_OPS_PROJECT_ID = ""
+EHR_OPS_DATASET_ID = ""
+
+# Add any other needed identifiers
+# -
+
+client = bigquery.Client(project=EHR_OPS_PROJECT_ID)
+
+# # Execute Queries
+
+# ## Query 1
+
+# Description: Pulls the first 10 rows in the measurement table
+
+# Define Query 1
+q1 = f"""
+    SELECT
+        *
+    FROM `{EHR_OPS_PROJECT_ID}.{EHR_OPS_DATASET_ID}.unioned_ehr_measurement`
+    LIMIT 10
+"""
+
+results1 = client.query(q1).to_dataframe()
+results1
+
+# ## Query 2
+
+# Description: Pulls the first 10 rows in the procedure_occurrence table
+
+# +
+# Define Query 2
+
+q2 = f"""
+    SELECT
+        *
+    FROM `{EHR_OPS_PROJECT_ID}.{EHR_OPS_DATASET_ID}.unioned_ehr_procedure_occurrence`
+    LIMIT 10   
+"""
+# -
+
+results2 = client.query(q2).to_dataframe()
+results2
---
+++ fhir_analysis/FHIR id analysis.py
@@ -0,0 +1,90 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Load Packages
+
+# +
+# Add any needed packages
+
+from google.cloud import bigquery
+# -
+
+# # Setup Connection
+
+# +
+# Fill in identifiers
+
+EHR_OPS_PROJECT_ID = ""
+EXPORT_DATASET_ID = ""
+
+# Add any other needed identifiers
+EXPORT_DATASET2_ID = ""
+# -
+
+client = bigquery.Client(project=EHR_OPS_PROJECT_ID)
+
+# # Execute Queries
+
+# ## FHIR Patient Resource identifiers
+
+# Description: Lists all identifiers associated with a FHIR Patient resource
+
+# Define Query 1
+q1 = f"""
+SELECT
+  id, identifier.system,
+  identifier.value, identifier.system,
+FROM `{EHR_OPS_PROJECT_ID}.{EXPORT_DATASET_ID}.Patient` p,
+UNNEST(p.identifier) AS identifier
+--WHERE p.id IN ('my_identifier_here')
+LIMIT 100
+"""
+
+results1 = client.query(q1).to_dataframe()
+results1
+
+# ## FHIR Patients across Submissions
+
+# Description: Identifies FHIR Patients submitted across submissions
+
+# +
+# Define Query 2
+
+q2 = f"""
+    WITH med_identifier1 AS (
+      SELECT DISTINCT
+        md.id
+      FROM `{EHR_OPS_PROJECT_ID}.{EXPORT_DATASET_ID}.Patient` md,
+        UNNEST(md.identifier) local_identifier
+    ),
+    med_identifier2 AS (
+      SELECT DISTINCT
+        md.id
+      FROM `{EHR_OPS_PROJECT_ID}.{EXPORT_DATASET2_ID}.Patient` md,
+        UNNEST(md.identifier) local_identifier
+    )
+    SELECT
+      id
+    FROM med_identifier2 md2
+    EXCEPT DISTINCT
+    SELECT
+      id
+    FROM  med_identifier1 md
+
+"""
+# -
+
+results2 = client.query(q2).to_dataframe()
+results2
